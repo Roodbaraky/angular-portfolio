9b001f8327fd70269c819b355e29b662e744d490
roodbaraky <kooroshr@hotmail.com>
2025-10-30 23:18:11 +0000

    Enhance navigation with section detection & add smooth scrolling

--- src/app/pages/home-page/home-page.ts
+++ src/app/pages/home-page/home-page.ts
@@ -1,9 +1,10 @@
-import {Component} from '@angular/core';
+import {Component, ElementRef, QueryList, signal, ViewChildren, WritableSignal} from '@angular/core';
 import {About} from '../../components/about/about';
 import {Experience} from '../../components/experience/experience';
 import {Projects} from '../../components/projects/projects';
 import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
 import {faGithub, faLinkedin} from '@fortawesome/free-brands-svg-icons';
+import {NgClass} from '@angular/common';
 
 @Component({
   selector: 'app-home-page',
@@ -11,7 +12,8 @@ import {faGithub, faLinkedin} from '@fortawesome/free-brands-svg-icons';
     FontAwesomeModule,
     About,
     Experience,
-    Projects
+    Projects,
+    NgClass
   ],
   templateUrl: './home-page.html',
   styleUrl: './home-page.css',
@@ -19,4 +21,32 @@ import {faGithub, faLinkedin} from '@fortawesome/free-brands-svg-icons';
 export class HomePage {
   faGithub = faGithub;
   faLinkedin = faLinkedin;
+
+  readonly sections = [
+    { id: 'about', label: 'About' },
+    { id: 'experience', label: 'Experience' },
+    { id: 'projects', label: 'Projects' },
+  ];
+
+
+  readonly currentSection: WritableSignal<string> = signal('none')
+
+  @ViewChildren('sectionElements') sectionElements!: QueryList<ElementRef<HTMLElement>>;
+
+  ngAfterViewInit() {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        for (const entry of entries) {
+          if (entry.isIntersecting) {
+            this.currentSection.set(entry.target.id);
+          }
+        }
+      },
+      { threshold: 0.7 }
+    );
+
+    for (const section of this.sectionElements) {
+      observer.observe(section.nativeElement);
+    }
+  }
 }
